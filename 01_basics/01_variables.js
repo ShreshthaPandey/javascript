a14a9addfea9b402d970899a44ad49aa39078d35
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-02 07:53:59 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "shreshthap2624@gmail.com"
+var accountPassward = " 123456"
+accountCity = "lko"
+
+// accountId = 2 not allowed
+accountEmail = "abc@gmail.com"
+accountPassward = "12121"
+accountCity = "bengal"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassward, accountCity])
