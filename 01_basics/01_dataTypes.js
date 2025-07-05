fba6155b6893b612a1766fdbca6de0bc6b14ba54
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-05 04:20:08 +0000

    conversion of datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,7 @@
+// alert(3 + 3) we are using node js not browser
+
+let name = "tanu"
+let age = 21
+let isLoggedIn = false
+
+console.log(typeof null);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33"
+let score1 = "tanu"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+let num = Number(score1)
+
+console.log(typeof ValueInNumber);
+console.log(typeof num)
+
