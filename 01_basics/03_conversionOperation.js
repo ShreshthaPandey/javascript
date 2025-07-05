cb2d92196f518078ea042cfd3bd933a9c2179cbc
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-05 05:09:38 +0000

    data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,33 @@
-let score = "33"
-let score1 = "tanu"
+// let score = "33"
+// let score1 = "tanu"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let ValueInNumber = Number(score)
-let num = Number(score1)
+// let ValueInNumber = Number(score)
+// let num = Number(score1)
 
-console.log(typeof ValueInNumber);
-console.log(typeof num)
+// console.log(typeof ValueInNumber);
+// console.log(typeof num)
 
+// ***************operation ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(3+2)
+
+let str1 = "tanu"
+let str = "pandey"
+
+let str3 = str1 + str
+console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+let gameCounter = 100;
+console.log(++gameCounter)
---
+++ 01_basics/05_nonPrimitiveDataTypes.js
@@ -0,0 +1,15 @@
+const name = ["tanu", "manu" , "dhanu"]
+ let myobj = {
+    name: "tanu",
+    age: 21,
+
+
+}
+
+const myfn = function(){
+    console.log("hello world")
+}
+console.log(myfn)
+console.log(name)
+console.log(typeof myfn)
+myfn();
