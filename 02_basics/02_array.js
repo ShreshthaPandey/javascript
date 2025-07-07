3ffddeb242cfc491fe9ade0c01a228a32ddbe0b9
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-07 05:32:25 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -17,4 +17,12 @@ const another_arr = [1,2,3 ,[4,5,6] , [7,8,9]]
 const real_arr = another_arr.flat(Infinity)
 console.log(real_arr)
 
+console.log(Array.from("hitesh")) //convert string to array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)) // make a new array and add them here
+
 
