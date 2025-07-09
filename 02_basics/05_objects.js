9dbe96824d8d5dd5dc344b1fa2a59cd095905b40
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-09 13:22:15 +0000

    object

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,25 @@
+ const course = {
+    name: "js in hindi",
+    price: "999",
+    couseInstructor: "hitesh"
+ }
+
+//   destructuring of object 
+
+ course.couseInstructor;  // method 1
+
+ const{couseInstructor} = course // method 2 ,, need not to write againa again..but can call aas many time we want
+
+ console.log(couseInstructor)
+
+//  API
+
+{ // if given no name then treated as jason(java script object notation)
+
+
+    // "name": "hitesh",
+    // "courseName": "js in hindi",
+    // "price" : "free"
+}
+
+ 
