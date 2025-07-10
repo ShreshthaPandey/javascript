8c2a71906d18cdef64898e0dd4b919ce37edb860
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-10 09:03:00 +0000

    jec

---
+++ 03_basics/06_execution_context.js
@@ -0,0 +1,10 @@
+// java execution context
+
+
+// 1) global execution Context => refer to variable(this) this ke value window context hoti hi
+
+// 2)  functional execution context 
+//3) eval execution useContext
+
+// {} => memory creation phase
+// 2nd => execution phase
