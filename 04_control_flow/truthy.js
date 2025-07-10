2441e9dc61926eeef1939fc47468b149ff1c6b49
Shreshtha Pandey <shreshthaP2624@gmail.com>
2025-07-10 10:18:30 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -36,4 +36,29 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0){
     console.log("obj is empty")
 }
+//********************************************************** 
 
+//nullish caescing operator (??) null undefined
+
+let val1;
+val1 = 5 ?? 10  // print first value => 5
+val2 = null ?? 10  // for safety check and print 2nd value
+
+var3 = undefined ?? 15
+
+
+
+console.log(val1)
+console.log(val2)
+console.log(var3)
+
+// this is basically use for null and undefined operator
+
+// *********************************************************
+
+// ternary operator
+
+// consition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("good amt") : console.log("dont want this chai")
